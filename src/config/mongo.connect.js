98ab7dfa2415fc86049b0b85e1ad67bf3de3ae14
Om Thakur <omprabhu2134@gmail.com>
2024-02-18 18:10:06 +0530

    connection done

--- src/config/mongo.connect.js
+++ src/config/mongo.connect.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
-// url  = "mongodb+srv://athelecity:vir7pnrNKeiQ82u5@athelecity.px2a8p1.mongodb.net/Athelecity?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
     console.log(`Connecting to ${url}`)
     return mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
     })
 }
 
 module.exports = connectDB
+
+
+// MONOGDB_URL = "mongodb+srv://OmThakur:0NXUekPu2Fipi8xa@syncinventor.hpsewul.mongodb.net/SyncInventor?retryWrites=true&w=majority"
+// PORT = 3000
